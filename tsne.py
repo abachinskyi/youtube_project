f3e3e164e80e1034ac29ee186e02cf48b1241e5d
Maksym Sukhorukov <makskant@gmail.com>
2017-05-15 01:42:29 +0300

    Update tsne.py
    current version, install all needed packages for correct use

--- tsne.py
+++ tsne.py
@@ -3,25 +3,28 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
-
+from wordcloud import WordCloud, STOPWORDS
+import os
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+STOPWORDS = list(STOPWORDS)
+STOPWORDS.append('I')
+STOPWORDS.append("I'm")
 def visualize(txtfile):
     model = fasttext.skipgram(txtfile, 'model')
     vectors = []
     for word in model.words:
         vectors.append(model[word])
-
     tsne = TSNE(n_components=2, random_state=0)
     np.set_printoptions(suppress=True)
     Y = tsne.fit_transform(vectors)
-    plt.figure(figsize=(20, 20))
+    fig = plt.figure(figsize=(20, 20))
     plt.scatter(Y[:, 0], Y[:, 1])
     for label, x, y in zip(model.words, Y[:, 0], Y[:, 1]):
         plt.annotate(label, xy=(x, y), xytext=(0, 0), textcoords='offset points')
-    plt.xlim(-16, 16)
-    plt.ylim(-15, 17)
-    plt.show()
+    fig.savefig(txtfile[:-4]+'1'+'.png')
 
-from wordcloud import WordCloud, STOPWORDS
 def cloud(txtfile):
     with open(txtfile) as f:
         lines = f.readlines()
@@ -31,8 +34,46 @@ def cloud(txtfile):
                           width=1200,
                           height=1000
                           ).generate(text)
+    fig = plt.figure()
     plt.imshow(wordcloud)
     plt.axis('off')
-    plt.show()
+    fig.savefig(txtfile[:-4] +'2'+ '.png')
+
+#filename = 'text_data/Orange.txt'
+
+def histogram(txtfile):
+    file = open(txtfile)
+    wordcount = {}
+    for word in file.read().lower().split():
+        if (word not in wordcount) and (word not in STOPWORDS):
+            wordcount[word] = 1
+        elif word not in STOPWORDS:
+            wordcount[word] += 1
+    final = dict((k, v) for k, v in wordcount.iteritems() if v > 5)
+    fig = plt.figure()
+    plt.bar(range(len(final)), final.values(), align='center')
+    plt.xticks(range(len(final)), final.keys(), rotation=85)
+    fig.savefig(txtfile[:-4] +'3'+'.png')
 
 
+first_dirs= ['text_data/'+x for x in os.listdir("text_data") if x.endswith('.txt')]
+second_dirs = ['text_data2/'+x for x in os.listdir("text_data2") if x.endswith('.txt')]
+
+for file in first_dirs:
+    visualize(file)
+    cloud(file)
+    histogram(file)
+
+for file in second_dirs:
+    visualize(file)
+    cloud(file)
+    histogram(file)
+
+
+#filen = 'text_data/Sushi.txt'
+
+#visualize(filen)
+#cloud(filen)
+#histogram(filen)
+
+#print first_dirs
